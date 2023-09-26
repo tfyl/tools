77f29cbf593552bc6f46a52ebcc745d82fb23fcc
bxd <2216403312@qq.com>
2023-09-26 09:02:19 +0800

    优化代码

--- go.mod
+++ go.mod
@@ -7,17 +7,7 @@ require (
 	gitee.com/baixudong/re v0.0.0-20230809033040-360c1d945e59
 	github.com/andybalholm/brotli v1.0.5
 	github.com/google/uuid v1.3.1
-	github.com/json-iterator/go v1.1.12
-	github.com/tidwall/gjson v1.16.0
 	golang.org/x/image v0.12.0
 	golang.org/x/net v0.15.0
 	golang.org/x/text v0.13.0
-)
-
-require (
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
 )
--- tools.go
+++ tools.go
@@ -8,24 +8,16 @@ import (
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/hmac"
 	"crypto/md5"
-	rand2 "crypto/rand"
 	"crypto/sha1"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/base64"
-	"encoding/pem"
 	"errors"
 	"fmt"
 	"hash"
 	"image"
 	"io"
 	"math"
-	"math/big"
 	"math/rand"
 
 	"net"
@@ -35,18 +27,13 @@ import (
 	"reflect"
 	"runtime"
 	"sort"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 	"unsafe"
 
-	"net/http"
-
 	"github.com/andybalholm/brotli"
 	"github.com/google/uuid"
-	jsoniter "github.com/json-iterator/go"
-	"github.com/tidwall/gjson"
 
 	_ "image/png"
 
@@ -58,16 +45,6 @@ import (
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding/simplifiedchinese"
 )
-
-//go:embed gospider.crt
-var CrtFile []byte
-
-//go:embed gospider.key
-var KeyFile []byte
-var jsonConfig = jsoniter.Config{
-	EscapeHTML:    true,
-	CaseSensitive: true,
-}.Froze()
 
 // 路径是否存在
 func PathExist(path string) bool {
@@ -146,42 +123,6 @@ func DecodeRead(txt io.Reader, code string) io.Reader {
 		txt = simplifiedchinese.GBK.NewDecoder().Reader(txt)
 	}
 	return txt
-}
-
-// 转成json
-func Any2json(data any) (result gjson.Result, err error) {
-	switch value := data.(type) {
-	case []byte:
-		result = gjson.ParseBytes(value)
-	case string:
-		result = gjson.Parse(value)
-	default:
-		marstr, err := JsonMarshal(value)
-		if err != nil {
-			return result, err
-		}
-		result = gjson.ParseBytes(marstr)
-	}
-	if !result.IsObject() && !result.IsArray() {
-		err = errors.New("不是json对象")
-	}
-	return
-}
-
-func JsonMarshal(data any) ([]byte, error) {
-	return jsonConfig.Marshal(data)
-}
-func JsonUnMarshal(data []byte, v any) error {
-	return jsonConfig.Unmarshal(data, v)
-}
-
-// 转成struct
-func Any2struct(data any, stru any) error {
-	con, err := JsonMarshal(data)
-	if err != nil {
-		return err
-	}
-	return JsonUnMarshal(con, stru)
 }
 
 // 合并两个结构体 *ci c2
@@ -315,34 +256,6 @@ func GetDefaultDir() (string, error) {
 // 拼接路径
 func PathJoin(elem ...string) string {
 	return filepath.Join(elem...)
-}
-func GetHost(addrTypes ...int) net.IP {
-	hosts := GetHosts(addrTypes...)
-	if len(hosts) == 0 {
-		return nil
-	} else {
-		return hosts[0]
-	}
-}
-func GetHosts(addrTypes ...int) []net.IP {
-	var addrType int
-	if len(addrTypes) > 0 {
-		addrType = addrTypes[0]
-	}
-	result := []net.IP{}
-	lls, err := net.InterfaceAddrs()
-	if err != nil {
-		return result
-	}
-	for _, ll := range lls {
-		mm, ok := ll.(*net.IPNet)
-		if ok && mm.IP.IsPrivate() {
-			if addrType == 0 || ParseIp(mm.IP) == addrType {
-				result = append(result, mm.IP)
-			}
-		}
-	}
-	return result
 }
 
 // aes加密
@@ -764,204 +677,7 @@ func CopyWitchContext(ctx context.Context, writer io.Writer, reader io.ReadClose
 	}
 	return
 }
-func ParseHost(host string) (net.IP, int) {
-	if ip := net.ParseIP(host); ip != nil {
-		if ip4 := ip.To4(); ip4 != nil {
-			return ip4, 4
-		} else if ip6 := ip.To16(); ip6 != nil {
-			return ip6, 6
-		}
-	}
-	return nil, 0
-}
-func ParseIp(ip net.IP) int {
-	if ip != nil {
-		if ip4 := ip.To4(); ip4 != nil {
-			return 4
-		} else if ip6 := ip.To16(); ip6 != nil {
-			return 6
-		}
-	}
-	return 0
-}
-func SplitHostPort(address string) (string, int, error) {
-	host, port, err := net.SplitHostPort(address)
-	if err != nil {
-		return "", 0, err
-	}
-	portnum, err := strconv.Atoi(port)
-	if err != nil {
-		return "", 0, err
-	}
-	if 1 > portnum || portnum > 0xffff {
-		return "", 0, errors.New("port number out of range " + port)
-	}
-	return host, portnum, nil
-}
 
-// 生成根证书
-func CreateRootCert(key *ecdsa.PrivateKey) (*x509.Certificate, error) {
-	beforDate, err := time.ParseInLocation(time.DateOnly, "2023-03-20", time.Local)
-	if err != nil {
-		return nil, err
-	}
-	afterDate, err := time.ParseInLocation(time.DateOnly, "3023-03-20", time.Local)
-	if err != nil {
-		return nil, err
-	}
-	rootCsr := &x509.Certificate{
-		Version:      3,
-		SerialNumber: big.NewInt(time.Now().Unix()),
-		Subject: pkix.Name{
-			Country:            []string{"CN"},
-			Province:           []string{"Shanghai"},
-			Locality:           []string{"Shanghai"},
-			Organization:       []string{"GoSpider"},
-			OrganizationalUnit: []string{"GoSpiderProxy"},
-			CommonName:         "Gospider Root CA",
-		},
-		NotBefore:             beforDate,
-		NotAfter:              afterDate,
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-		MaxPathLen:            1,
-		MaxPathLenZero:        false,
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
-	}
-	rootDer, err := x509.CreateCertificate(rand2.Reader, rootCsr, rootCsr, key.Public(), key)
-	if err != nil {
-		return nil, err
-	}
-	return x509.ParseCertificate(rootDer)
-}
-
-// 生成私钥
-func CreateCertKey() (*ecdsa.PrivateKey, error) {
-	return ecdsa.GenerateKey(elliptic.P256(), rand2.Reader)
-}
-func GetCertWithCN(rootCert *x509.Certificate, key *ecdsa.PrivateKey, commonName string) (*x509.Certificate, error) {
-	csr := &x509.Certificate{
-		Version:               3,
-		SerialNumber:          big.NewInt(time.Now().Unix()),
-		Subject:               rootCert.Subject,
-		IPAddresses:           []net.IP{net.IPv4(127, 0, 0, 1)},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(1000, 0, 0),
-		BasicConstraintsValid: true,
-		IsCA:                  false,
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-	}
-	csr.IPAddresses = []net.IP{}
-	if commonName != "" {
-		if ip, ipType := ParseHost(commonName); ipType == 0 {
-			csr.Subject.CommonName = commonName
-			csr.DNSNames = []string{commonName}
-		} else {
-			csr.IPAddresses = append(csr.IPAddresses, ip)
-		}
-	}
-	der, err := x509.CreateCertificate(rand2.Reader, csr, rootCert, key.Public(), key)
-	if err != nil {
-		return nil, err
-	}
-	return x509.ParseCertificate(der)
-}
-
-func CreateCertWithCert(rootCert *x509.Certificate, key *ecdsa.PrivateKey, preCert *x509.Certificate) (*x509.Certificate, error) {
-	if preCert.DNSNames == nil && preCert.Subject.CommonName != "" {
-		preCert.DNSNames = []string{preCert.Subject.CommonName}
-	}
-	rootCert.Subject.CommonName = preCert.Subject.CommonName
-	csr := &x509.Certificate{
-		Version:               3,
-		SerialNumber:          big.NewInt(time.Now().Unix()),
-		Subject:               rootCert.Subject,
-		DNSNames:              preCert.DNSNames,
-		IPAddresses:           preCert.IPAddresses,
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(1000, 0, 0),
-		BasicConstraintsValid: true,
-		IsCA:                  false,
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-	}
-	if len(preCert.DNSNames) > 0 {
-		csr.Subject.CommonName = preCert.DNSNames[0]
-	}
-	der, err := x509.CreateCertificate(rand2.Reader, csr, rootCert, key.Public(), key)
-	if err != nil {
-		return nil, err
-	}
-	return x509.ParseCertificate(der)
-}
-func CreateProxyCertWithName(serverName string) (tlsCert tls.Certificate, err error) {
-	crt, err := LoadCert(CrtFile)
-	if err != nil {
-		return tlsCert, err
-	}
-	key, err := LoadCertKey(KeyFile)
-	if err != nil {
-		return tlsCert, err
-	}
-	cert, err := GetCertWithCN(crt, key, serverName)
-	if err != nil {
-		return tlsCert, err
-	}
-	return GetTlsCert(cert, key)
-}
-func CreateProxyCertWithCert(crt *x509.Certificate, key *ecdsa.PrivateKey, preCert *x509.Certificate) (tlsCert tls.Certificate, err error) {
-	if crt == nil {
-		crt, err = LoadCert(CrtFile)
-		if err != nil {
-			return tlsCert, err
-		}
-	}
-	if key == nil {
-		key, err = LoadCertKey(KeyFile)
-		if err != nil {
-			return tlsCert, err
-		}
-	}
-	cert, err := CreateCertWithCert(crt, key, preCert)
-	if err != nil {
-		return tlsCert, err
-	}
-	return GetTlsCert(cert, key)
-}
-func GetTlsCert(cert *x509.Certificate, key *ecdsa.PrivateKey) (tls.Certificate, error) {
-	keyFile, err := GetCertKeyData(key)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-	return tls.X509KeyPair(GetCertData(cert), keyFile)
-}
-func GetCertData(cert *x509.Certificate) []byte {
-	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})
-}
-func GetCertKeyData(key *ecdsa.PrivateKey) ([]byte, error) {
-	keyDer, err := x509.MarshalECPrivateKey(key)
-	if err != nil {
-		return nil, err
-	}
-	return pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDer}), nil
-}
-func LoadCertKey(data []byte) (*ecdsa.PrivateKey, error) {
-	block, _ := pem.Decode(data)
-	return x509.ParseECPrivateKey(block.Bytes)
-}
-func LoadCert(data []byte) (*x509.Certificate, error) {
-	block, _ := pem.Decode(data)
-	return x509.ParseCertificate(block.Bytes)
-}
-
-func GetServerName(addr string) string {
-	host, _, err := net.SplitHostPort(addr)
-	if err != nil {
-		return addr
-	}
-	return host
-}
 func CopySlices[T any](value []T) []T {
 	copyValue := make([]T, len(value))
 	copy(copyValue, value)
@@ -973,10 +689,6 @@ func CopySlicess[T any](value [][]T) [][]T {
 		copyValue[i] = CopySlices(value[i])
 	}
 	return copyValue
-}
-
-func GetContentTypeWithBytes(content []byte) string {
-	return http.DetectContentType(content)
 }
 
 func Uuid() uuid.UUID {
@@ -1004,16 +716,4 @@ func ImgDiffer(c, c2 []byte) (float64, error) {
 	}
 	score /= math.Pow(2, 16) * math.Pow(float64(bounds.Dx()), 2) * math.Pow(float64(bounds.Dy()), 2)
 	return score, nil
-}
-func VerifyProxy(proxyUrl string) (*url.URL, error) {
-	proxy, err := url.Parse(proxyUrl)
-	if err != nil {
-		return nil, err
-	}
-	switch proxy.Scheme {
-	case "http", "socks5", "https":
-		return proxy, nil
-	default:
-		return nil, err
-	}
 }
